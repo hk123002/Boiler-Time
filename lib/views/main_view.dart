9a01d8ffb55d02a7242265679efa2ce03e8eaa60
hk123002 <89869612+hk123002@users.noreply.github.com>
2022-09-25 06:25:18 -0400

    navigtionbar added

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -12,6 +12,34 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Market',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Community',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Chat',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Setting',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +71,36 @@ class _MainViewState extends State<MainView> {
           )
         ],
       ),
-      body: const Text("Hello, World!"),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            backgroundColor: Colors.deepOrange,
+            icon: Icon(Icons.add_business),
+            label: 'Market',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.deepOrange,
+            icon: Icon(Icons.monitor_sharp),
+            label: 'Community',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.deepOrange,
+            icon: Icon(Icons.message),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.deepOrange,
+            icon: Icon(Icons.miscellaneous_services),
+            label: 'Setting',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
