88a319f8779cc22270f2ed825406f23f98ae1b48
hk123002 <89869612+hk123002@users.noreply.github.com>
2022-10-15 20:38:57 -0400

    home about routes added

---
+++ lib/views/home/about/academic_schedule.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AcademicSchedule extends StatelessWidget {
+  const AcademicSchedule({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('acaemic schedule'),
+      ),
+    );
+  }
+}
---
+++ lib/views/home/about/bus_schedule.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BusSchedule extends StatelessWidget {
+  const BusSchedule({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('bus schedule'),
+      ),
+    );
+  }
+}
