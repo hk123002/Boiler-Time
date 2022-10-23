1cc4414fdf4861d0a30b190eb9313d34687c17dc
Jiho1211 <90180259+Jiho1211@users.noreply.github.com>
2022-10-22 22:51:26 -0400

    Category 2

---
+++ lib/views/community/alumni.dart
@@ -0,0 +1,183 @@
+import 'package:boiler_time/constants/routes.dart';
+import 'package:boiler_time/services/auth/auth_service.dart';
+import 'package:boiler_time/views/community/postPage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+import '../../enums/menu_action.dart';
+import '../main_view.dart';
+
+class Alumni extends StatefulWidget {
+  const Alumni({super.key});
+
+  @override
+  State<Alumni> createState() => _communityState();
+}
+
+//final _post = FirebaseFirestore.instance.collection('post');
+final CollectionReference _alumni =
+    FirebaseFirestore.instance.collection('alumni');
+
+class _communityState extends State<Alumni> {
+// text fields' controllers
+  final TextEditingController _title = TextEditingController();
+  final TextEditingController _content = TextEditingController();
+
+  Future<void> _create([DocumentSnapshot? documentSnapshot]) async {
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _title,
+                  decoration: const InputDecoration(labelText: 'Title'),
+                ),
+                TextField(
+                  controller: _content,
+                  decoration: const InputDecoration(labelText: 'Content'),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  child: const Text('Create'),
+                  onPressed: () async {
+                    final String title = _title.text;
+                    final String content = _content.text;
+
+                    await _alumni.add({"Title": title, "Content": content});
+
+                    _title.text = '';
+                    _content.text = '';
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<void> _update([DocumentSnapshot? documentSnapshot]) async {
+    if (documentSnapshot != null) {
+      _title.text = documentSnapshot['Title'];
+      _content.text = documentSnapshot['Content'].toString();
+    }
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _title,
+                  decoration: const InputDecoration(labelText: 'Title'),
+                ),
+                TextField(
+                  controller: _content,
+                  decoration: const InputDecoration(labelText: 'Content'),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  child: const Text('Update'),
+                  onPressed: () async {
+                    final String title = _title.text;
+                    final String content = _content.text;
+
+                    await _alumni.add({"Title": title, "Content": content});
+
+                    _title.text = '';
+                    _content.text = '';
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<void> _delete(String alumniID) async {
+    await _alumni.doc(alumniID).delete();
+
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('You have successfully deleted a product')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Alumni'),
+        ),
+        //body: ListView.builder(
+        //itemCount: 5,
+        //itemBuilder: (BuildContext context, int index) {
+        body: StreamBuilder(
+          stream: _alumni.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                      streamSnapshot.data!.docs[index];
+                  return Card(
+                    margin: const EdgeInsets.all(10),
+                    child: ListTile(
+                      title: Text(documentSnapshot['Title']),
+                      subtitle: Text(documentSnapshot['Content']),
+                      trailing: SizedBox(
+                        width: 100,
+                        child: Row(
+                          children: [
+                            IconButton(
+                                icon: const Icon(Icons.edit),
+                                onPressed: () => _update(documentSnapshot)),
+                            IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () => _delete(documentSnapshot.id)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+// Add new product
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _create(),
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
+  }
+}
