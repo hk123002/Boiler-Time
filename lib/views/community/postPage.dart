c9a857d53f4e0c9a08b65e2c30d65c71466736e1
Jiho1211 <90180259+Jiho1211@users.noreply.github.com>
2022-11-05 06:31:53 -0400

    community comment

--- lib/views/community/postPage.dart
+++ lib/views/community/postPage.dart
@@ -1,9 +1,14 @@
 import 'package:boiler_time/constants/routes.dart';
 import 'package:boiler_time/services/auth/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:comment_tree/data/comment.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
 import '../../enums/menu_action.dart';
+import 'package:animated_tree_view/animated_tree_view.dart';
+
+final CollectionReference _exam = FirebaseFirestore.instance.collection('post');
 
 class PostPage extends StatefulWidget {
   final String documentID;
@@ -29,12 +34,155 @@ class _postPageViewState extends State<PostPage> {
     super.dispose();
   }
 
+  final TextEditingController _comment = TextEditingController();
+
+  Future<void> _create([DocumentSnapshot? documentSnapshot]) async {
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _comment,
+                  decoration: const InputDecoration(labelText: 'Comment'),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  child: const Text('Create'),
+                  onPressed: () async {
+                    final String comment = _comment.text;
+
+                    await _exam.add({"Comment": comment});
+
+                    _comment.text = '';
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<void> _update([DocumentSnapshot? documentSnapshot]) async {
+    if (documentSnapshot != null) {
+      _comment.text = documentSnapshot['Content'].toString();
+    }
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _comment,
+                  decoration: const InputDecoration(labelText: 'Comment'),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  child: const Text('Update'),
+                  onPressed: () async {
+                    final String comment = _comment.text;
+
+                    await _exam.add({"Comment": comment});
+
+                    _comment.text = '';
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<void> _delete(String examID) async {
+    await _exam.doc(examID).delete();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('You have successfully deleted a post')));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(documentID.toString() + " " + collectionName.toString()),
-      ),
-    );
+        appBar: AppBar(
+          toolbarHeight: 40,
+          title: const Text('Post'),
+        ),
+        body: StreamBuilder(
+          stream: _exam.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                      streamSnapshot.data!.docs[index];
+                  return Column(children: [
+                    Card(
+                      margin: const EdgeInsets.all(10),
+                      child: ListTile(
+                        title: Text(documentSnapshot['Title']),
+                        subtitle: Text(documentSnapshot['Content']),
+                      ),
+                    ),
+                    Card(
+                      margin: const EdgeInsets.all(10),
+                      child: ListTile(
+                        //title: Text(documentSnapshot['Commnet']),
+                        trailing: SizedBox(
+                          width: 100,
+                          child: Row(
+                            children: [
+                              IconButton(
+                                  icon: const Icon(Icons.edit),
+                                  onPressed: () => _update(documentSnapshot)),
+                              IconButton(
+                                  icon: const Icon(Icons.delete),
+                                  onPressed: () =>
+                                      _delete(documentSnapshot.id)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]);
+                },
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+// Add new product
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _create(),
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
   }
 }
