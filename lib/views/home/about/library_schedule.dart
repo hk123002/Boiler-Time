84949ac1ae6003914a8ee82718013441112686e0
seaungwon <albertvin785@gmail.com>
2022-11-10 00:40:50 -0500

    library updated

--- lib/views/home/about/library_schedule.dart
+++ lib/views/home/about/library_schedule.dart
@@ -75,7 +75,8 @@ class LibraryTime extends StatelessWidget {
                 color: Color.fromARGB(169, 52, 45, 53),
               ),
               child: const ExpansionTile(
-                title: Text('Parrish Library of Management & Economics'),
+                title:
+                    Text('Roland G. Parrish Library of Management & Economics'),
                 subtitle: Text('403 W State St'),
                 leading: Icon(
                   Icons.info,
@@ -97,7 +98,202 @@ class LibraryTime extends StatelessWidget {
                 color: Color.fromARGB(169, 52, 45, 53),
               ),
               child: const ExpansionTile(
-                title: Text('West Lafayette LIbrary'),
+                title: Text('Archives and Special Collections'),
+                subtitle: Text('208 W Columbia St'),
+                leading: Icon(
+                  Icons.info,
+                  size: 30,
+                ),
+                children: [
+                  ListTile(title: Text('Sunday'), subtitle: Text('Closed')),
+                  ListTile(
+                      title: Text('Monday - Friday'),
+                      subtitle: Text('10.00 AM - 4:30 PM')),
+                  ListTile(title: Text('Saturday'), subtitle: Text('Closed')),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(169, 52, 45, 53),
+              ),
+              child: const ExpansionTile(
+                title: Text('Aviation and Transportation Technology'),
+                subtitle: Text('208 W Columbia St'),
+                leading: Icon(
+                  Icons.info,
+                  size: 30,
+                ),
+                children: [
+                  ListTile(title: Text('Sunday'), subtitle: Text('Closed')),
+                  ListTile(
+                      title: Text('Monday - Friday'),
+                      subtitle: Text('8.00 AM - 5:00 PM')),
+                  ListTile(title: Text('Saturday'), subtitle: Text('Closed')),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(169, 52, 45, 53),
+              ),
+              child: const ExpansionTile(
+                title: Text('Black Cultural Center Library'),
+                subtitle: Text('208 W Columbia St'),
+                leading: Icon(
+                  Icons.info,
+                  size: 30,
+                ),
+                children: [
+                  ListTile(
+                      title: Text('Sunday'),
+                      subtitle: Text('2:00 PM - 9:00 PM')),
+                  ListTile(
+                      title: Text('Monday - Friday'),
+                      subtitle: Text('8.00 AM - 5:00 PM')),
+                  ListTile(title: Text('Saturday'), subtitle: Text('Closed')),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(169, 52, 45, 53),
+              ),
+              child: const ExpansionTile(
+                title:
+                    Text('Humanities, Social Sciences and Education Library'),
+                subtitle: Text('208 W Columbia St'),
+                leading: Icon(
+                  Icons.info,
+                  size: 30,
+                ),
+                children: [
+                  ListTile(
+                      title: Text('Sunday'),
+                      subtitle: Text('1:00 PM - 12:00 AM')),
+                  ListTile(
+                      title: Text('Monday - Friday'),
+                      subtitle: Text('8.00 AM - 12:00 AM')),
+                  ListTile(
+                      title: Text('Saturday'),
+                      subtitle: Text('1:00 PM - 5:00 PM')),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(169, 52, 45, 53),
+              ),
+              child: const ExpansionTile(
+                title: Text('Interlibrary Loan'),
+                subtitle: Text('208 W Columbia St'),
+                leading: Icon(
+                  Icons.info,
+                  size: 30,
+                ),
+                children: [
+                  ListTile(title: Text('Sunday'), subtitle: Text('Closed')),
+                  ListTile(
+                      title: Text('Monday - Friday'),
+                      subtitle: Text('8.00 AM - 5:00 PM')),
+                  ListTile(title: Text('Saturday'), subtitle: Text('Closed')),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(169, 52, 45, 53),
+              ),
+              child: const ExpansionTile(
+                title: Text('Mathematical Sciences Library'),
+                subtitle: Text('208 W Columbia St'),
+                leading: Icon(
+                  Icons.info,
+                  size: 30,
+                ),
+                children: [
+                  ListTile(
+                      title: Text('Sunday'),
+                      subtitle: Text('1:00 PM - 8:00 PM')),
+                  ListTile(
+                      title: Text('Monday - Friday'),
+                      subtitle: Text('8.00 AM - 8:00 PM')),
+                  ListTile(
+                      title: Text('Saturday'),
+                      subtitle: Text('1:00 PM - 5:00 PM')),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(169, 52, 45, 53),
+              ),
+              child: const ExpansionTile(
+                title: Text('Veterinary Medical Library'),
+                subtitle: Text('208 W Columbia St'),
+                leading: Icon(
+                  Icons.info,
+                  size: 30,
+                ),
+                children: [
+                  ListTile(
+                      title: Text('Sunday'),
+                      subtitle: Text('1:00 PM - 8:00 PM')),
+                  ListTile(
+                      title: Text('Monday - Friday'),
+                      subtitle: Text('8.00 AM - 8:00 PM')),
+                  ListTile(
+                      title: Text('Saturday'),
+                      subtitle: Text('1:00 PM - 5:00 PM')),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(169, 52, 45, 53),
+              ),
+              child: const ExpansionTile(
+                title: Text('Library of Engineering and Science'),
+                subtitle: Text('208 W Columbia St'),
+                leading: Icon(
+                  Icons.info,
+                  size: 30,
+                ),
+                children: [
+                  ListTile(
+                      title: Text('Sunday'),
+                      subtitle: Text('11:00 AM - 2:00 PM')),
+                  ListTile(
+                      title: Text('Monday - Friday'),
+                      subtitle: Text('7.00 AM - 2:00 AM')),
+                  ListTile(
+                      title: Text('Saturday'),
+                      subtitle: Text('11:00 AM - 12:00 AM')),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(169, 52, 45, 53),
+              ),
+              child: const ExpansionTile(
+                title: Text('West Lafayette Library'),
                 subtitle: Text('208 W Columbia St'),
                 leading: Icon(
                   Icons.info,
